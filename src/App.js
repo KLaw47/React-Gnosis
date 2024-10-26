127c55e068ad3e888c66a74c539115778dd8478f
Keaton Law <keaton.c.law@gmail.com>
2024-10-26 16:57:37 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn or Die
         </a>
       </header>
     </div>
