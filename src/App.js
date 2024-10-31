4ade83520956151a4b81db2e19c7a83fb4712aea
Keaton Law <keaton.c.law@gmail.com>
2024-10-30 19:48:27 -0500

    added contact form, necessary to connect to node mailer on the back end?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,9 +13,17 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} /> {/* Contact Route */}
           {/* Add routes for other pages */}
         </Routes>
       </main>
+
+      <footer>
+        <p>
+          <a href="/contact" className="footer-link">Contact</a>
+        </p>
+      </footer>
+      
     </div>
   );
 }
