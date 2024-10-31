210ca6bcec7af74bd8d8043d5c2d2339c6aef81c
Keaton Law <keaton.c.law@gmail.com>
2024-10-31 11:39:52 -0500

    merge

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const transporter = require('./mailer');
 const app = express();
 const PORT = process.env.BACKEND_PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 
 //basic route to test server
 app.get('/', (req, res) => {
